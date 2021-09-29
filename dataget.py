16870f8e28b9b81f41ad460d5f454ab537f9bc99
sn49 <34567701+sn49@users.noreply.github.com>
2021-09-29 22:48:00 +0900

    패턴 필터링 ## > #

--- dataget.py
+++ dataget.py
@@ -1,7 +1,9 @@
 import urllib.request as ul
 import json
 from urllib.parse import quote
+import re
 
+from discord import message
 
 def get_chat_data():
     url = f"http://127.0.0.1:8000/discord"
@@ -21,15 +23,28 @@ def get_chat_data():
 
     return "fail"
 
+blackwordfile = open("blackword.txt", "r", encoding="UTF-8")
+blackwordlist = tuple(blackwordfile.read().split("\n"))
+blackwordfile.close()
 
-def CheckMessage(message):
-    blackwordfile = open("blackword.txt", "r", encoding="UTF-8")
+filtpattFile=open("blackregex.txt", "r", encoding="UTF-8")
+filtpattList = tuple(filtpattFile.read().split("\n"))
+filtpattFile.close()
 
-    blackwordlist = blackwordfile.read().split("\n")
-    blackwordfile.close()
+def CheckMessage(mess):
+    global blackwordlist
+    global filtpattList
+
+    mess = mess.replace("          ", "")
+
+    for black in filtpattList:
+        mess=re.sub(black,"#",mess)
 
     for black in blackwordlist:
-        if black in message:
-            message = message.replace(black, "##")
+        if black in mess:
+            mess = mess.replace(black, "#")
+    
 
-    return message
+    
+
+    return mess
